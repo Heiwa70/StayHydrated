090a7dd8ced35c51eb17143a0b3c28ad86d88907
Clément <clement70200@gmail.com>
2023-03-30 09:53:14 +0200

    Initial Commit

---
+++ StayHydrated/StayHydratedApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayHydratedApp.swift
+//  StayHydrated
+//
+//  Created by Clément Seiler on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct StayHydratedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
