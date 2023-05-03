9d7e26747a2abf40b2bc53ba3cd2231b0ba21a38
Peachouw <paulblanchet25@gmail.com>
2023-05-03 14:23:48 +0200

    view created

--- StayHydrated/ContentView.swift
+++ StayHydrated/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  StayHydrated
 //
-//  Created by Paul Blanchet on 30/03/2023.
+//  Created by Paul Blanche on 30/03/2023.
 //
 
 import SwiftUI
---
+++ StayHydrated/View/FirstLogView.swift
@@ -0,0 +1,20 @@
+//
+//  FirstLogView.swift
+//  StayHydrated
+//
+//  Created by Paul Blanchet on 03/05/2023.
+//
+
+import SwiftUI
+
+struct FirstLogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FirstLogView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstLogView()
+    }
+}
