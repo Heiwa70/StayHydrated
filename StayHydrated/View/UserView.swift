31ed8be76d6f6b157c1f87df577c9ee47d6bae34
Peachouw <paulblanchet25@gmail.com>
2023-05-17 14:40:12 +0200

    commit jsp pq clement la pute

--- StayHydrated/View/UserView.swift
+++ StayHydrated/View/UserView.swift
@@ -8,8 +8,89 @@
 import SwiftUI
 
 struct UserView: View {
+    @State private var age: Int = 24
+    @State private var taille:Int=180
+    @State private var poids:Int = 75
+    var activity = ["Tennis", "Badminton", "Football", "Hockey", "Golf" , "Rugby", "Course", "Vélo", "Autre"]
+    @State private var selectedActivity = "Autre"
+    @State private var autoObj = true
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image(systemName: "checkmark")
+                .resizable()
+                .frame(width: 40, height: 40)
+                .foregroundColor(.white)
+                .padding(20)
+                .background(Color.green)
+                .clipShape(Circle())
+            
+            Spacer()
+            
+            Text("Paul")
+            
+            Spacer()
+            
+            HStack{
+                Text("Age : ")
+                    .frame(width: 170,alignment: .trailing)
+                    
+                TextField("Enter your name", value: $age, format : .number)
+                    .frame(width: 100)
+
+                    .textFieldStyle(.roundedBorder)
+                                    .padding()
+            }
+            HStack{
+                Text("Taille : ")
+                    .frame(width: 170,alignment: .trailing)
+
+                TextField("Enter your name", value: $taille, format : .number)
+                    .frame(width: 100)
+
+                    .textFieldStyle(.roundedBorder)
+                                    .padding()
+                
+            }
+            HStack{
+                Text("Poids : ")
+                    .frame(width: 170,alignment: .trailing)
+
+                TextField("Enter your name", value: $poids, format : .number)
+                    .frame(width: 100)
+
+                    .textFieldStyle(.roundedBorder)
+                                    .padding()
+                
+            }
+            
+            HStack{
+                Text("Activité principale : ")
+                    .frame(width: 170,alignment: .trailing)
+
+                Picker("Please choose a color", selection: $selectedActivity) {
+                    ForEach(activity, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .frame(width: 100)
+                .clipped()
+            }
+            Spacer()
+
+            HStack{
+                Text("Objectif : ")
+                
+                HStack{
+                    Toggle("Auto", isOn: $autoObj)
+
+                }.frame(width: 100)
+            }
+            
+            
+            
+        }
     }
 }
 
